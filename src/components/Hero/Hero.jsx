2b5a968bf4e12cf82057d9ad917c4abd1556435b
moraeseduarda <eduardamorads@gmail.com>
2025-02-10 15:34:00 -0300

    initial commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+import "./Hero.css"
+import { MaquinaDeEscrever } from "../MaquinaDeEscrever";
+
+const Hero = () => {
+  return (
+    <section className="hero-container">
+      <div className="hero-content">
+        <h2><MaquinaDeEscrever text="Front End Developer">
+
+        </MaquinaDeEscrever></h2>
+        <p>Creating Websites with Pixel</p>
+      </div>
+
+      <div className="hero-img">
+        <div>
+          <div className="tech-icon">
+            <img></img>
+          </div>
+          <img src="src\img\hero-img.jpg"></img>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react"
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="main-nav">
+      <div className="nav-content">
+        <div className="logo">EDUARDA</div>
+        <ul>
+          <li>
+            <a className="menu-item">Sobre</a>
+          </li>
+          <li>
+            <a className="menu-item">Projetos</a>
+          </li>
+          <li>
+            <a className="menu-item">Habilidades</a>
+          </li>
+
+          <button className="contact-btn" onClick={() => {}}>
+            Contato
+          </button>
+
+          <button className="menu-btn" onClick={() => {}}>
+            <span
+            class={"material-symbols-outlined"}
+            style={{ fontSize: "1.8rem" }}
+            >
+              menu
+            </span>
+          </button>
+
+        </ul>
+      </div>
+    </nav>
+
+  )
+}
+
+export default Navbar
